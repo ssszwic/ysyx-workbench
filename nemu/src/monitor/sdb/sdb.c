2032fde071274a4183df5ed75e8773e9107d7820
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-15 18:24:05 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:24:05 up  2:44,  1 user,  load average: 0.62, 0.39, 0.22

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -119,15 +119,13 @@ void sdb_mainloop() {
     extern void sdl_clear_event_queue();
     sdl_clear_event_queue();
 #endif
-    printf("2\n");
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { printf("ok\n"); return; }
+        if (cmd_table[i].handler(args) < 0) { return; }
         break;
       }
     }
-    printf("3\n");
 
     if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
   }
