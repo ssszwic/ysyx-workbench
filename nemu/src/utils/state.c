fcdff1cbe0b5963656430b110046e86375633de6
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-16 11:33:38 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:33:38 up  2:02,  1 user,  load average: 0.76, 0.40, 0.29

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -19,13 +19,13 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
   // when type 'q' without run 'c', nemu_state.state = NEMU_STOP, return 1
-  int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-    (nemu_state.state == NEMU_QUIT);
+  // int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
+  //   (nemu_state.state == NEMU_QUIT);
 
   // ssszw change 2022.1016
   // allow program to exit when STOP state
   // int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
   //   (nemu_state.state == NEMU_QUIT) || (nemu_state.state == NEMU_STOP && nemu_state.halt_ret == 0);
   // end change
-  return !good;
+  return 3;
 }
