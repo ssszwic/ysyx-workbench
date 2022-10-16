9c2aa4b470fa79ef125fccb43b267e6aa69473c9
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-16 19:25:20 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:20 up  9:54,  1 user,  load average: 0.75, 0.41, 0.21

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -222,14 +222,16 @@ static int cmd_x(char *args) {
   paddr_t addr;
 
   sscanf(first, "%[0-9]", num_str);
+  printf("%s", num_str);
   if (strlen(num_str) != strlen(first)) {
     printf("Num must be integer greater than 0\n");
     return 0;
   }
   sscanf(num_str, "%d", &num);
 
-  sscanf(first, "%[0-9]", num_str);
-  if (strlen(addr_str) < (strlen(first) - 1)) {
+  sscanf(second, "%[0-9]", addr_str);
+  printf("%s", addr_str);
+  if (strlen(addr_str) < (strlen(second) - 1)) {
     printf("Addr must be hexadecimal integer\n");
     return 0;
   }
