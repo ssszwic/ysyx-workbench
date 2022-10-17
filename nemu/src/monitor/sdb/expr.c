feed579c7968500cd1988be463e6e0ca94666a8e
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 16:30:53 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:30:53 up 1 day,  6:59,  1 user,  load average: 0.19, 0.30, 0.26

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -37,7 +37,7 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
-  {"+", '+'},         // plus
+  {"\\+", '+'},         // plus
   {" -", TK_NOTYPE},    // spaces
   {"\\-", '-'},           // sub
   // {" *", TK_NOTYPE},    // spaces
