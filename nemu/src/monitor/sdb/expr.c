6c4875b266235566bea9f9da2490bb4397a9003c
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-18 20:26:49 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:26:49 up 10:40,  1 user,  load average: 0.10, 0.31, 0.29

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -337,22 +337,32 @@ word_t expr(char *e, bool *success) {
     printf("%u\n", result);
   }
 
+  // test
   FILE *fp = fopen("/home/ssszw/Work/ysyx-workbench/nemu/tools/gen-expr/input", "r");
-  char result_test[40] = {};
+  uint32_t test_result = 0;
+  char test_str[40] = {};
   char buf[1000] = {};
+  int i = 0;
   while (1) {
-    if (fgets(result_test, sizeof(result_test), fp) == NULL) {
+    if (fgets(test_str, sizeof(test_str), fp) == NULL) {
       break;
     }
     if (fgets(buf, sizeof(buf), fp) == NULL) {
       break;
     }
-    printf("%s", result_test);
-    printf("%s", buf);
-    printf("\n");
+    if (!make_token(e)) {
+      printf("matched failed! at %d\n", i);
+      break;
+    }
+    eval_success = true;
+    test_result = eval(0, nr_token-1);
+    if(!eval_success || (test_result != strtoul(test_str, NULL, 10))) {
+      printf("cal error at %d\n", i);
+      break;
+    }
+    i++;
   }
   fclose(fp);
-  
 
 
   
