1bc721a373a491b833ac96afdb52d20178b9e1e5
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-19 13:45:32 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:45:32 up  1:46,  1 user,  load average: 0.46, 0.34, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -52,8 +52,9 @@ static struct rule {
   {"&&", TK_AND},               // AND
   {"!=", TK_NOEQ},              // un equal
   {"==", TK_EQ},                // equal
-  {"[0-9]+", TK_DEC},           // dec num (consider '-' when eval)
   {"0[xX][0-9]+", TK_HEX},     // hex num (consider '-' when eval)
+  {"[0-9]+", TK_DEC},           // dec num (consider '-' when eval)
+  
 
 };
 
