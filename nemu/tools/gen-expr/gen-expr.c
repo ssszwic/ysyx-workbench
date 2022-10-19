affd1eb3772681f69c47dc64526b47f8bc18f812
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-19 12:38:26 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:38:26 up 39 min,  1 user,  load average: 0.41, 0.40, 0.34

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -55,9 +55,9 @@ static void gen_rand_op() {
     case 1: strcat(buf, "-"); strcat(buf_unsign, "-");break;
     case 2: strcat(buf, "*"); strcat(buf_unsign, "*");break;
     case 3: strcat(buf, "/"); strcat(buf_unsign, "/");break;
-    case 4: strcat(buf, "&&"); strcat(buf_unsign, "/");break;
-    case 5: strcat(buf, "=="); strcat(buf_unsign, "/");break;
-    case 6: strcat(buf, "!="); strcat(buf_unsign, "/");break;
+    case 4: strcat(buf, "&&"); strcat(buf_unsign, "&&");break;
+    case 5: strcat(buf, "=="); strcat(buf_unsign, "==");break;
+    case 6: strcat(buf, "!="); strcat(buf_unsign, "!=");break;
     default: assert(0);
   }
 }
@@ -104,10 +104,10 @@ int main(int argc, char *argv[]) {
     len = 1;
     gen_rand_expr();
 
-    // buf[0] = '\0';
-    // buf_unsign[0] = '\0';
-    // strcat(buf, "3&&2");
-    // strcat(buf_unsign, "3&&2");
+    buf[0] = '\0';
+    buf_unsign[0] = '\0';
+    strcat(buf, "   (      1613001108  ==667548417   )      ");
+    strcat(buf_unsign, "   (      1613001108  ==667548417   )      ");
     
     sprintf(code_buf, code_format, buf_unsign);
 
