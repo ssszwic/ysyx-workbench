d0c13a200c3d4eaf55c5ddb5ee8789ec261aac46
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 17:56:52 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:56:52 up  8:13,  1 user,  load average: 0.48, 0.41, 0.32

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,12 +41,14 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   // scan watchpoint
+#ifdef CONFIG_WATCHPOINT
   if(update_wp(MUXDEF(CONFIG_ITRACE, _this->logbuf, NULL), !g_print_step)) {
     // only state is RUNNING, stop excute and print watchpoint
     if (nemu_state.state == NEMU_RUNNING) {
       nemu_state.state = NEMU_STOP;
     }
   }
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
