a7dc307bdcb5731cce5cdd761d9b9f2e9d7ea095
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-22 14:12:04 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:12:04 up  4:33,  1 user,  load average: 0.57, 0.36, 0.27

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -23,17 +23,20 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S, TYPE_J,
+  TYPE_R, TYPE_I, TYPE_S, TYPE_B, TYPE_U, TYPE_J, 
   TYPE_N, // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
+#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) | \
+                            (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | \
                             (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1); } while(0)
-#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+
 
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -43,10 +46,13 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
   int rs2 = BITS(i, 24, 20);
   *dest = rd;
   switch (type) {
+    case TYPE_R: src1R(); src2R();         break;
     case TYPE_I: src1R();          immI(); break;
+    case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_J:                   immJ(); break;
-    case TYPE_S: src1R(); src2R(); immS(); break;
+    
   }
 }
 
@@ -65,10 +71,14 @@ static int decode_exec(Decode *s) {
   INSTPAT_START();
 
   /*----------------------------------------- R -----------------------------------------*/
+  // add word  addw rd, rs1, rs2
+  INSTPAT("0000000 ????? ????? 000 ????? 01110 11", addw   , R, R(dest) = SEXT(BITS(src1 + src2, 31, 0), 32));
 
   /*----------------------------------------- I -----------------------------------------*/
   // load double word (8 byte)
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + imm, 8));
+  // load single word (4 byte)
+  INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(dest) = Mr(src1 + imm, 4));
   // junp and link register 
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(dest) = s->snpc; s->dnpc = src1 + imm; );
   // add immediate  addi rd, rs1, imm[11:0]
@@ -80,7 +90,7 @@ static int decode_exec(Decode *s) {
   
   /*----------------------------------------- B -----------------------------------------*/
   // branch equal  
-  // INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, Mw(src1 + imm, 8, src2));
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, s->dnpc = (src1 == src2) ? (s->pc + imm) : s->snpc);
 
   /*----------------------------------------- U -----------------------------------------*/
   // load upper immediate
