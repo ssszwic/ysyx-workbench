9f2890367a73cb1c6461612b76af51374cd4efb0
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 12:57:08 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:57:08 up  2:36,  1 user,  load average: 0.51, 0.28, 0.20

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -4,16 +4,21 @@
 extern char _heap_start;
 int main(const char *args);
 
+// start and end of heep
+// A section of memory freely available to the program
 Area heap = RANGE(&_heap_start, PMEM_END);
+
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
+// print 1 char
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
 
+// ending running of the program
 void halt(int code) {
   nemu_trap(code);
 
@@ -21,6 +26,7 @@ void halt(int code) {
   while (1);
 }
 
+// initialize turing machine
 void _trm_init() {
   int ret = main(mainargs);
   halt(ret);
