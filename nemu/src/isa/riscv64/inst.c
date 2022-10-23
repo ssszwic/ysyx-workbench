eaa0dd84fe910abde8acf835ffea030cc58966b7
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 10:40:14 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:40:14 up 19 min,  1 user,  load average: 0.11, 0.22, 0.15

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -128,9 +128,11 @@ static int decode_exec(Decode *s) {
   // shift left logical immediate
   INSTPAT("000000? ????? ????? 001 ????? 00100 11", slli   , I, R(dest) = src1 << BITS(imm, 5, 0));
   // shift right arithmetic immediate
-  INSTPAT("010000? ????? ????? 101 ????? 00100 11", srai   , I, R(dest) = (int64_t) src1 >> BITS(imm, 5, 0)); // all shift is logic for unsigned
+  INSTPAT("010000? ????? ????? 101 ????? 00100 11", srai   , I, R(dest) = (int64_t) src1 >> BITS(imm, 5, 0)); // convert to signed, all shift is logical for unsigned
   // shift right logical immediate
   INSTPAT("000000? ????? ????? 101 ????? 00100 11", srli   , I, R(dest) = src1 >> BITS(imm, 5, 0)); // all shift is logic for unsigned
+  // shift left logical immediate word
+  INSTPAT("0000000 ????? ????? 001 ????? 00110 11", slliw  , I, R(dest) = SEXT(BITS(src1 << BITS(imm, 4, 0), 31, 0), 32));
 
   /*----------------------------------------- S -----------------------------------------*/
   // store double word (8 byte)
