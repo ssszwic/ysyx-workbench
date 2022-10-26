72ea37987fe6568259132e2c4be30259a4606cd4
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 09:54:33 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:54:33 up 21 min,  1 user,  load average: 0.29, 0.25, 0.19

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -74,17 +74,17 @@ void sdl_clear_event_queue() {
 }
 
 void init_device() {
-  // IFDEF(CONFIG_TARGET_AM, ioe_init());
+  IFDEF(CONFIG_TARGET_AM, ioe_init());
   // // 
-  // init_map();
+  init_map();
 
-  // IFDEF(CONFIG_HAS_SERIAL, init_serial());
-  // IFDEF(CONFIG_HAS_TIMER, init_timer());
-  // IFDEF(CONFIG_HAS_VGA, init_vga());
-  // IFDEF(CONFIG_HAS_KEYBOARD, init_i8042());
-  // IFDEF(CONFIG_HAS_AUDIO, init_audio());
-  // IFDEF(CONFIG_HAS_DISK, init_disk());
-  // IFDEF(CONFIG_HAS_SDCARD, init_sdcard());
+  IFDEF(CONFIG_HAS_SERIAL, init_serial());
+  IFDEF(CONFIG_HAS_TIMER, init_timer());
+  IFDEF(CONFIG_HAS_VGA, init_vga());
+  IFDEF(CONFIG_HAS_KEYBOARD, init_i8042());
+  IFDEF(CONFIG_HAS_AUDIO, init_audio());
+  IFDEF(CONFIG_HAS_DISK, init_disk());
+  IFDEF(CONFIG_HAS_SDCARD, init_sdcard());
 
-  // IFNDEF(CONFIG_TARGET_AM, init_alarm());
+  IFNDEF(CONFIG_TARGET_AM, init_alarm());
 }
