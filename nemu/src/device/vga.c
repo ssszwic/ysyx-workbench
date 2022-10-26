d400bbb44fe76713e67a36662230de6b87b947df
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 10:19:00 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:19:00 up 46 min,  1 user,  load average: 0.34, 0.35, 0.30

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -60,6 +60,7 @@ static inline void update_screen() {
   SDL_RenderClear(renderer);
   // SDL_RenderCopy(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
+  SDL_TLSCleanup();
 }
 #else
 static void init_screen() {}
