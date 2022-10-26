079f060aa9b6ddc9473c8a40ddcdacbbe53d1278
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 17:15:46 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:15:46 up  7:42,  1 user,  load average: 0.60, 0.48, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,12 +4,12 @@
 int printf(const char *fmt, ...);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t data = (uint32_t) inl(KBD_ADDR);
-  if (data != 0) {
-    printf("data: %04x\n", data);
-  }
+  // if (data != 0) {
+  //   printf("data: %04x\n", data);
+  // }
   
-  kbd->keydown = 1;
-  kbd->keycode = 0;
-  // kbd->keydown = (data & 0xff00) == 0x8000;
-  // kbd->keycode = data;
+  // kbd->keydown = 1;
+  // kbd->keycode = 0;
+  kbd->keydown = (data & 0xff00) == 0x8000;
+  kbd->keycode = data;
 }
