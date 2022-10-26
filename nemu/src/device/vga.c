8256bfeeb990216b84f618d185dc40ed1a165cac
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 18:16:43 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:16:43 up  8:43,  1 user,  load average: 0.49, 0.38, 0.23

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -47,10 +47,10 @@ static void init_screen() {
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
   SDL_Init(SDL_INIT_VIDEO);
   SDL_CreateWindowAndRenderer(
-      SCREEN_W * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
-      SCREEN_H * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
+      SCREEN_W,
+      SCREEN_H,
       0, &window, &renderer);
-  // SDL_SetWindowTitle(window, title);
+  SDL_SetWindowTitle(window, title);
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
 }
