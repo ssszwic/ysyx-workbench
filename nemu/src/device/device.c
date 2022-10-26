a38d42617ddc39dbd89e6d720f30d3020dc78ca3
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 09:55:32 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:55:32 up 22 min,  1 user,  load average: 0.50, 0.31, 0.21

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -74,12 +74,11 @@ void sdl_clear_event_queue() {
 }
 
 void init_device() {
-  // IFDEF(CONFIG_TARGET_AM, ioe_init());
-  // // // 
-  // init_map();
+  IFDEF(CONFIG_TARGET_AM, ioe_init());
+  init_map();
 
-  // IFDEF(CONFIG_HAS_SERIAL, init_serial());
-  // IFDEF(CONFIG_HAS_TIMER, init_timer());
+  IFDEF(CONFIG_HAS_SERIAL, init_serial());
+  IFDEF(CONFIG_HAS_TIMER, init_timer());
   // IFDEF(CONFIG_HAS_VGA, init_vga());
   IFDEF(CONFIG_HAS_KEYBOARD, init_i8042());
   IFDEF(CONFIG_HAS_AUDIO, init_audio());
