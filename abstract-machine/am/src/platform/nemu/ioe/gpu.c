26c6ad0ddef6451e030c97122b0d7402894a28e9
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 21:48:15 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:48:15 up 12:15,  1 user,  load average: 1.20, 0.74, 0.56

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,13 +4,13 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  uint32_t data = (uint32_t) inl(VGACTL_ADDR);
-  int width = data >> 16;
-  int height = data & 0xff00;
-  int i;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < width * height; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  // uint32_t data = (uint32_t) inl(VGACTL_ADDR);
+  // int width = data >> 16;
+  // int height = data & 0xff00;
+  // int i;
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < width * height; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
