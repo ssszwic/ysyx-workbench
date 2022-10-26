132c83e64242ae4e0db83e3a9ca533f840fb5890
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 21:29:24 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:29:24 up 11:56,  1 user,  load average: 0.35, 0.39, 0.32

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -68,7 +68,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   sprintf(tmp, "----> read \t%s\t" FMT_PADDR "\t%02d", map->name, addr, len);
   strcpy(d_ring_buf[d_ring_ref], tmp);
 #endif
-
+  printf("%s, %d\n", map->name, len);
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
@@ -89,7 +89,6 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
-  printf("%s %d\n", map->name, len);
   invoke_callback(map->callback, offset, len, true);
 }
 
