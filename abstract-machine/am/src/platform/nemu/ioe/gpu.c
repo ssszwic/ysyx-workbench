292908cdecb893223b6cf880eef21bfa3c170c8b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-27 11:02:30 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:30 up 27 min,  1 user,  load average: 0.33, 0.43, 0.37

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,13 +4,14 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 int printf(const char *fmt, ...);
 void __am_gpu_init() {
-  uint32_t data = (uint32_t) inl(VGACTL_ADDR);
-  int width = data >> 16;
-  int height = data & 0x0000ffff;
-  int i;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < width * height; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  // only for test initial
+  // uint32_t data = (uint32_t) inl(VGACTL_ADDR);
+  // int width = data >> 16;
+  // int height = data & 0x0000ffff;
+  // int i;
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < width * height; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -27,21 +28,6 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t data = (uint32_t) inl(VGACTL_ADDR);
   int width = data >> 16;
   int height = data & 0x0000ffff;
-
-  // uint32_t *addr = NULL;
-  // uint32_t *pixels_tmp = (uint32_t *) ctl->pixels;
-  // // check out-of-bounds
-  // if((ctl->x + ctl->w > width) || (ctl->y + ctl->h > height)) {
-  //   panic("out of display range");
-  // }
-  // // transform pixel data
-  // for (int j = 0; j < ctl->h; j++) {
-  //   addr = (uint32_t *) FB_ADDR + width * (j + ctl->y) + ctl->x;
-  //   for (int i = 0; i < ctl->w; i++) {
-  //     outl((uintptr_t) addr++, *pixels_tmp++);
-  //   }
-  // }
-
   if((ctl->x + ctl->w > width) || (ctl->y + ctl->h > height)) {
     panic("out of display range");
   }
