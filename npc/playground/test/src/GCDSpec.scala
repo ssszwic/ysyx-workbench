3343552f5dc4195b7a9c22f060cac8c12fb0e72d
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-04 13:58:45 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:58:45 up  4:12,  1 user,  load average: 0.44, 0.38, 0.26

--- npc/playground/test/src/GCDSpec.scala
+++ npc/playground/test/src/GCDSpec.scala
@@ -18,30 +18,8 @@ import utest._
 object GCDSpec extends ChiselUtestTester {
   val tests = Tests {
     test("GCD") {
-      testCircuit(new DecoupledGcd(16)) {
-        dut =>
-          dut.input.initSource()
-          dut.input.setSourceClock(dut.clock)
-          dut.output.initSink()
-          dut.output.setSinkClock(dut.clock)
-          val testValues = for {x <- 0 to 10; y <- 0 to 10} yield (x, y)
-          val inputSeq = testValues.map { case (x, y) => (new GcdInputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U) }
-          val resultSeq = testValues.map { case (x, y) =>
-            (new GcdOutputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U, _.gcd -> BigInt(x).gcd(BigInt(y)).U)
-          }
-          fork {
-            // push inputs into the calculator, stall for 11 cycles one third of the way
-            val (seq1, seq2) = inputSeq.splitAt(resultSeq.length / 3)
-            dut.input.enqueueSeq(seq1)
-            dut.clock.step(11)
-            dut.input.enqueueSeq(seq2)
-          }.fork {
-            // retrieve computations from the calculator, stall for 10 cycles one half of the way
-            val (seq1, seq2) = resultSeq.splitAt(resultSeq.length / 2)
-            dut.output.expectDequeueSeq(seq1)
-            dut.clock.step(10)
-            dut.output.expectDequeueSeq(seq2)
-          }.join()
+      testCircuit(new IFU) { dut =>
+          
       }
     }
   }
