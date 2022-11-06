e7c11793e13861b10a6ccafe74d7f84e3ce00d85
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 16:40:25 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:40:25 up 1 day,  6:21,  1 user,  load average: 0.70, 0.49, 0.52

--- npc/playground/src/main/Elaborate.scala
+++ npc/playground/src/main/Elaborate.scala
@@ -1,13 +1,15 @@
 package main
 import circt.stage._
+import chisel3.stage.ChiselStage
 
 object Elaborate extends App {
   def top = new ALU
-  val useMFC = true // use MLIR-based firrtl compiler
-  val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  if (useMFC) {
-    (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
-  } else {
-    (new chisel3.stage.ChiselStage).execute(args, generator)
-  }
+  // val useMFC = true // use MLIR-based firrtl compiler
+  // val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
+  // if (useMFC) {
+  //   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+  // } else {
+  //   (new chisel3.stage.ChiselStage).execute(args, generator)
+  // }
+  (new ChiselStage).emitVerilog(top, args)
 }
