5ed6632e6b85fa8b305096ecc9c3098145907af6
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-09 19:12:08 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:12:08 up  9:28,  1 user,  load average: 0.93, 0.63, 0.34

--- npc/playground/src/main/Top.scala
+++ npc/playground/src/main/Top.scala
@@ -10,14 +10,6 @@ class Top extends Module {
     // inst Memory
     val instData  = Input(UInt(64.W))
     val instAddr  = Output(UInt(64.W))
-    // memory
-    val rData     = Input(UInt(64.W))
-    val rAddr     = Output(UInt(64.W))
-    val wData     = Output(UInt(64.W))
-    val wAddr     = Output(UInt(64.W))
-    val wen       = Output(Bool())
-    val ren       = Output(Bool())
-    val length    = Output(UInt(2.W))
   })
 
   // declare module
@@ -25,7 +17,7 @@ class Top extends Module {
   val IDUInst = Module(new IDU)
   val RegFilesInst = Module(new RegFiles)
   val ALUInst = Module(new ALU)
-  val MemExtendsInst = Module(new MemExtends)
+  val MemCtrlInst = Module(new MemCtrl)
 
   // default nextpc = pc + 4
   val nextpcDefault = Wire(UInt(64.W))
@@ -33,17 +25,19 @@ class Top extends Module {
 
   // IO
   io.instAddr := IFUInst.io.pc
-  io.rAddr    := ALUInst.io.result
-  io.wData    := RegFilesInst.io.rs2Data
-  io.wAddr    := ALUInst.io.result
-  io.wen      := IDUInst.io.wenMem
-  io.ren      := IDUInst.io.renMem
-  io.length   := IDUInst.io.lengthMem
+
+  // MemCtrlInst
+  MemCtrlInst.io.wData  := RegFilesInst.io.rs2Data
+  MemCtrlInst.io.addr   := ALUInst.io.result
+  MemCtrlInst.io.wen    := IDUInst.io.wenMem
+  MemCtrlInst.io.ren    := IDUInst.io.renMem
+  MemCtrlInst.io.length := IDUInst.io.lengthMem
+  MemCtrlInst.io.unsign := IDUInst.io.unsignMem
 
   // IFU
   IFUInst.io.nextpc   := Mux(ALUInst.io.nextpcSel || IDUInst.io.jumpSel, ALUInst.io.result, nextpcDefault)
   IFUInst.io.instGet  := io.instData
-  IFUInst.io.pcEn    := io.cpuEn
+  IFUInst.io.pcEn     := io.cpuEn
 
   // IDU
   IDUInst.io.inst := IFUInst.io.inst
@@ -54,7 +48,7 @@ class Top extends Module {
   RegFilesInst.io.wen     := IDUInst.io.wenReg
   RegFilesInst.io.wAddr   := IDUInst.io.rdAddr
   RegFilesInst.io.wData   := Mux(IDUInst.io.jumpSel, nextpcDefault, 
-                                  Mux(IDUInst.io.renMem, MemExtendsInst.io.result, ALUInst.io.result))
+                                  Mux(IDUInst.io.renMem, MemCtrlInst.io.rData, ALUInst.io.result))
 
   // ALU
   ALUInst.io.rs1  := RegFilesInst.io.rs1Data
@@ -62,9 +56,4 @@ class Top extends Module {
   ALUInst.io.imme := IDUInst.io.imme
   ALUInst.io.pc   := IFUInst.io.pc
   ALUInst.io_alu  <> IDUInst.io_alu
-
-  // MemExtends
-  MemExtendsInst.io.data      := io.rData
-  MemExtendsInst.io.lengthMem := IDUInst.io.lengthMem
-  MemExtendsInst.io.unsignMem := IDUInst.io.unsignMem
 }
