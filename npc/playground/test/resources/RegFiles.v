25cc3fafd4020f4d4558d7c3405ef9be1b91eab3
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-10 21:46:02 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:46:02 up 11:20,  1 user,  load average: 0.31, 0.42, 0.34

--- npc/playground/test/resources/RegFiles.v
+++ npc/playground/test/resources/RegFiles.v
@@ -8,23 +8,25 @@ module RegFiles(
   // data
   input     [63:0]  wData,
   output    [63:0]  rs1Data,
-  output    [63:0]  rs2Data,
-)
+  output    [63:0]  rs2Data
+);
 
 reg   [63:0]  regFiles [31:0];
+genvar i;
 
-always@(posedge clock) begin
-  if(reset) begin
-    for(integer i = 0; i <16; i = i + 1) begin
+generate 
+  for(i = 0; i < 32; i = i + 1) begin
+    if(reset) begin
 	    regFiles[i] <= 64'b0;
     end
-  end
-  else begin
-    if(wen) begin
-      regFiles[wAddr] <= 64'b0;
+    else if(wen & (wAddr == i)) begin
+      regFiles[i] <= wData;
+    end
+    else begin
+      regFiles[i] <= regFiles[i];
     end
   end
-end
+endgenerate
 
 assign rs1Data = (rs1Addr == 5'd0) ? 64'd0 : regFiles[rs1Addr];
 assign rs2Data = (rs2Addr == 5'd0) ? 64'd0 : regFiles[rs2Addr];
