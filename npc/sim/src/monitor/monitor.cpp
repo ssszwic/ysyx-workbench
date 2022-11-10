4e86747645fa429fc6555f544127f0cab577933c
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-10 17:24:05 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:24:05 up  6:58,  1 user,  load average: 0.10, 0.28, 0.32

---
+++ npc/sim/src/monitor/monitor.cpp
@@ -0,0 +1,35 @@
+#include <getopt.h>
+#include <stdio.h>
+
+static char *img_file = NULL;
+static char *elf_file = NULL;
+
+static int parse_args(int argc, char *argv[]) {
+  const struct option table[] = {
+    {"batch"    , no_argument      , NULL, 'b'},
+    {"help"     , no_argument      , NULL, 'h'},
+    {"elf"      , required_argument, NULL, 'e'},
+    {0          , 0                , NULL,  0 },
+  };
+  int o;
+  while ( (o = getopt_long(argc, argv, "bh", table, NULL)) != -1) {
+    switch (o) {
+      case 'b': break;
+      case 'l': log_file = optarg; break;
+      case 'd': diff_so_file = optarg; break;
+      case 'e': elf_file = optarg; break;
+      // all command-line options have been parsed, return -1
+      case 1: img_file = optarg; return 0;
+      default:
+        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+        printf("\t-b,--batch              run with batch mode\n");
+        printf("\t-l,--log=FILE           output log to FILE\n");
+        printf("\t-e,--elf=ELF_FILE       run function trace\n");
+        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+        printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\n");
+        exit(0);
+    }
+  }
+  return 0;
+}
