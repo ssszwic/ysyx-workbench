f674fae76456566aa86eb11bb5d534fb175541b8
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-11 17:53:02 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:53:02 up  7:13,  1 user,  load average: 0.53, 1.11, 1.06

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -52,8 +52,8 @@ app: $(BINARY)
 
 $(BINARY): $(OBJS) $(ARCHIVES)
 	@echo + LD $@
-	echo 666666666666666666
-	echo $(LD) -o $@ $(OBJS) $(LDFLAGS) $(ARCHIVES) $(LIBS)
+	echo 666666666666
+	echo $(LIBS)
 	$(LD) -o $@ $(OBJS) $(LDFLAGS) $(ARCHIVES) $(LIBS)
 
 clean:
