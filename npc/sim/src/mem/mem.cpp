c61a5e5fbf03f746b1b40a40b14d107b9f1b2d75
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-11 15:09:22 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:09:22 up  4:29,  1 user,  load average: 0.72, 0.63, 0.45

--- npc/sim/src/mem/mem.cpp
+++ npc/sim/src/mem/mem.cpp
@@ -34,7 +34,7 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
   out_of_bound(paddr);
 }
 
-extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
+extern "C" void pmem_write(long long waddr, long long wdata, uint8_t wmask) {
   // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
