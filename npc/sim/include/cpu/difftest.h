3323731489dc8fbb82b8125aa74450800c30c439
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 18:14:33 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:14:33 up 1 day,  7:57,  1 user,  load average: 1.01, 0.64, 0.48

--- npc/sim/include/cpu/difftest.h
+++ npc/sim/include/cpu/difftest.h
@@ -8,7 +8,7 @@ enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
 extern void (*ref_difftest_regcpy)(void *dut, bool direction);
 extern void (*ref_difftest_exec)(uint64_t n);
-extern void (*ref_difftest_raise_intr)(uint64_t NO);
+// extern void (*ref_difftest_raise_intr)(uint64_t NO);
 extern void (*ref_difftest_init)();
 
 // CPU state is different between nemu and npc
