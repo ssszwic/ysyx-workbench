f4e5f677482c3ebacd96e77f0fc61a99b0b1f0a7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 20:24:00 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:24:00 up 1 day, 10:06,  1 user,  load average: 0.40, 0.59, 0.64

--- npc/sim/src/monitor/monitor.cpp
+++ npc/sim/src/monitor/monitor.cpp
@@ -170,7 +170,7 @@ void log_config() {
 
   log_write(true, ANSI_FMT("DIFFTEST: ", ANSI_FG_BLUE));
 #ifdef CONFIG_DIFFTEST
-  log_write(true, ANSI_FMT("666", ANSI_FG_GREEN));
+  log_write(true, ANSI_FMT("ON\n", ANSI_FG_GREEN));
   printf("-5");
 #else
   log_write(true, ANSI_FMT("OFF\n", ANSI_FG_YELLOW));
