693b424616b1d4486634042e8fe692a83a9809a3
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-14 20:57:54 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:57:54 up 2 days, 10:40,  1 user,  load average: 1.50, 0.64, 0.52

--- npc/playground/src/main/alu/Div.scala
+++ npc/playground/src/main/alu/Div.scala
@@ -35,8 +35,8 @@ class Div extends Module {
     }
   }
 
-  val result = Wire(SInt(66.W))
-  result := data1Tmp.asSInt / data1Tmp.asSInt
+  val result = Wire(SInt(65.W))
+  result := data1Tmp.asSInt / data2Tmp.asSInt
 
   when(io.wordSel) {
     io.result := Cat(Fill(32, result(32)), result(31, 0))
