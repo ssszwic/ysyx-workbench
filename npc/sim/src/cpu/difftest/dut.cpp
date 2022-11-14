6ddbe8b8ea7e732965eca64b1c9643d11ac9b99c
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-14 11:25:42 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:25:42 up 2 days,  1:08,  1 user,  load average: 0.88, 0.70, 0.77

--- npc/sim/src/cpu/difftest/dut.cpp
+++ npc/sim/src/cpu/difftest/dut.cpp
@@ -77,7 +77,7 @@ void init_difftest(char *ref_so_file, long img_size) {
   printf("2%lx\n", *cpu.pc);
   cpu_diff.pc = *cpu.pc;
   printf("3%lx\n", cpu_diff.pc);
-  ref_difftest_regcpy(&cpu_diff, DIFFTEST_TO_DUT);
+  ref_difftest_regcpy(&cpu_diff, DIFFTEST_TO_REF);
   printf("4%lx\n", *cpu.pc);
 }
 
