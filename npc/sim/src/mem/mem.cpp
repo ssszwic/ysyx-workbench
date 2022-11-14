05b4da8d3c023bbb3ecf7d73ba81edd3bda2a447
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-14 21:29:54 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:29:54 up 2 days, 11:12,  1 user,  load average: 0.43, 0.55, 0.63

--- npc/sim/src/mem/mem.cpp
+++ npc/sim/src/mem/mem.cpp
@@ -78,7 +78,7 @@ extern "C" void pmem_write(long long waddr, long long wdata, uint8_t wmask) {
     char tmp[50] = {};
     memset(mem_ring_buf[mem_ring_ref], ' ', 6);
     if (++mem_ring_ref == MEM_RING_BUF_WIDTH) {mem_ring_ref = 0;}
-    sprintf(tmp, "----> write\t0x%016llx\t0x%016llx\t0x%02x", waddr, wdata, wmask);
+    sprintf(tmp, "----> write\t0x%016llx\t0x%016llx\t0x%04x", waddr, wdata, wmask);
     strcpy(mem_ring_buf[mem_ring_ref], tmp);
   }
   flip = !flip;
