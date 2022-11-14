d005a015f2bd001b81fb997352a8b68c22c0573a
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-14 14:52:57 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:52:57 up 2 days,  4:35,  1 user,  load average: 0.44, 0.41, 0.48

--- npc/sim/src/cpu/reg.cpp
+++ npc/sim/src/cpu/reg.cpp
@@ -12,7 +12,7 @@ const char *regs[] = {
 void isa_reg_display(bool *err_list) {
   // pc reg
   printf(ANSI_FMT("CPU register state: \n", ANSI_FG_BLUE));
-  printf("pc:  0x%016lx\t  ", *npc_cpu.pc);
+  printf("pc:  0x%016lx\t  ", npc_cpu.pc);
   if(err_list[33]) {
     printf(ANSI_FMT("npc: 0x%016lx\n", ANSI_FG_RED), npc_cpu.next_pc);
   }
@@ -40,7 +40,7 @@ uint64_t isa_reg_str2val(const char *s, bool *success) {
   // pc reg
   if (strcmp(s, "pc") == 0){
     *success = true;
-    return *npc_cpu.pc;
+    return npc_cpu.pc;
   }
   // noemal reg
   int i = 0;
