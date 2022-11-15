77b92304525db802158eeef2b533457f004cc2d0
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-15 20:15:33 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:15:33 up  7:02,  1 user,  load average: 0.94, 0.78, 0.56

--- npc/sim/src/device/timer.cpp
+++ npc/sim/src/device/timer.cpp
@@ -41,7 +41,7 @@ static void timer_io_handler(uint32_t offset, uint8_t mask, bool is_write) {
 void init_timer() {
   boot_time = get_time_internal();
   timer_base = (uint32_t *) new_space(32);
-  add_mmio_map("serial", CONFIG_TIMER_MMIO, timer_base, 8, (io_callback_t) timer_io_handler);
+  add_mmio_map("timer", CONFIG_TIMER_MMIO, timer_base, 8, (io_callback_t) timer_io_handler);
 }
 
 
