bea863870ca1073aab6af35360efaaeffce5019e
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-16 20:25:51 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:25:51 up 1 day,  7:13,  1 user,  load average: 0.10, 0.28, 0.50

--- npc/sim/include/config.h
+++ npc/sim/include/config.h
@@ -3,17 +3,17 @@
 
 // switch
 // #define CONFIG_WAVE
-#define CONFIG_ITRACE
+// #define CONFIG_ITRACE
 // #define CONFIT_WATCHPOINT //depend CONFIG_ITRACE
 // #define CONFIG_MEMORY_TRACE
 // #define CONFIG_FUNCTION_TRACE
-#define CONFIG_DIFFTEST
+// #define CONFIG_DIFFTEST
 
-#define CONFIG_DEVICE
-#define CONFIG_HAS_SERIAL
-#define CONFIG_HAS_TIMER
-#define CONFIG_HAS_KEYBOARD
-#define CONFIG_HAS_VGA
+// #define CONFIG_DEVICE
+// #define CONFIG_HAS_SERIAL
+// #define CONFIG_HAS_TIMER
+// #define CONFIG_HAS_KEYBOARD
+// #define CONFIG_HAS_VGA
 
 // mem
 #define CONFIG_MSIZE 0x8000000
