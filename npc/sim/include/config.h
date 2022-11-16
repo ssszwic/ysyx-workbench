b921f4f39f975c42d644eae21765ce9bb4fa50c3
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-16 13:00:17 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:00:17 up 23:47,  1 user,  load average: 0.84, 0.96, 0.85

---
+++ npc/sim/include/config.h
@@ -0,0 +1,23 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// switch
+// #define CONFIG_WAVE
+#define CONFIG_ITRACE
+// #define CONFIT_WATCHPOINT //depend CONFIG_ITRACE
+#define CONFIG_MEMORY_TRACE
+// #define CONFIG_FUNCTION_TRACE
+#define CONFIG_DIFFTEST
+#define CONFIG_DEVICE
+#define CONFIG_HAS_SERIAL
+#define CONFIG_HAS_TIMER
+
+// mem
+#define CONFIG_MSIZE 0x8000000
+#define CONFIG_MBASE 0x80000000
+
+// device
+#define CONFIG_SERIAL_MMIO 0xa00003f8
+#define CONFIG_TIMER_MMIO 0xa0000048
+
+#endif
