cc6e11e94a68c1e6bfba2854b62a621a6992d107
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-16 13:37:28 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:37:28 up 1 day, 24 min,  1 user,  load average: 0.52, 0.62, 0.78

--- abstract-machine/am/src/riscv/npc/npc.h
+++ abstract-machine/am/src/riscv/npc/npc.h
@@ -6,6 +6,6 @@
 // device
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
 #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
-#define KBD_ADDR        (DEVICE_BASE + 0x0000060)
+#define KBD_ADDR        (DEVICE_BASE + 0x0000080) // different form nemu
 
 #endif
