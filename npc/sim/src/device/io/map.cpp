d8844c07d520e4923b87d4cccff98e0b0591f4d9
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-16 16:20:55 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:20:55 up 1 day,  3:08,  1 user,  load average: 0.47, 0.40, 0.46

--- npc/sim/src/device/io/map.cpp
+++ npc/sim/src/device/io/map.cpp
@@ -42,7 +42,7 @@ static void invoke_callback(io_callback_t c, uint32_t offset, uint8_t mask, bool
 uint64_t map_read(paddr_t addr, IOMap *map) {
   check_bound(map, addr);
   uint32_t offset = addr - map->low;
-  printf("offset\n");
+  printf("read\n");
   invoke_callback(map->callback, offset, 0, false); // prepare data to read
   
   word_t ret = host_read((uint8_t *) map->space + offset, 8);
@@ -51,6 +51,7 @@ uint64_t map_read(paddr_t addr, IOMap *map) {
 
 void map_write(paddr_t addr, uint64_t data, uint8_t mask, IOMap *map) {
   check_bound(map, addr);
+  printf("write\n");
   paddr_t offset = addr - map->low;
   assert(offset % 8 == 0);
   // write to map
