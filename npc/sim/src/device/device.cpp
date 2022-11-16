00f6e1bf6c25c002aa469ecf89f48a470f371655
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-16 14:29:46 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:29:46 up 1 day,  1:17,  1 user,  load average: 0.64, 0.54, 0.54

--- npc/sim/src/device/device.cpp
+++ npc/sim/src/device/device.cpp
@@ -8,15 +8,31 @@ void init_serial();
 void init_map();
 void init_timer();
 void init_i8042();
-
+void init_vga();
 
 
 void send_key(uint8_t, bool);
 void vga_update_screen();
 
+void init_device() {
+  init_map();
+  #ifdef CONFIG_HAS_SERIAL
+  init_serial();
+  #endif
+  #ifdef CONFIG_HAS_TIMER
+  init_timer();
+  #endif
+  #ifdef CONFIG_I8042_DATA_MMIO
+  init_i8042();
+  #endif
+  #ifdef CONFIG_HAS_VGA
+  init_vga();
+  #endif
+}
+
 void device_update() {
 
-#ifndef CONFIG_HAS_VGA
+#ifdef CONFIG_HAS_VGA
   vga_update_screen();
 #endif
 
@@ -49,15 +65,3 @@ void sdl_clear_event_queue() {
 #endif
 }
 
-void init_device() {
-  init_map();
-  #ifdef CONFIG_HAS_SERIAL
-  init_serial();
-  #endif
-  #ifdef CONFIG_HAS_TIMER
-  init_timer();
-  #endif
-  #ifdef CONFIG_I8042_DATA_MMIO
-  init_i8042();
-  #endif
-}
