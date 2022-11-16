7997a842f40e62d2473129fb5e63013377391df1
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-16 15:48:11 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:48:11 up 1 day,  2:35,  1 user,  load average: 0.91, 0.66, 0.44

--- npc/sim/src/device/keyboard.cpp
+++ npc/sim/src/device/keyboard.cpp
@@ -68,7 +68,7 @@ void send_key(uint8_t scancode, bool is_keydown) {
 
 static uint32_t *i8042_data_port_base = NULL;
 
-static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
+static void i8042_data_io_handler(uint32_t offset, uint8_t mask, bool is_write) {
   assert(!is_write);
   assert(offset == 0);
   i8042_data_port_base[0] = key_dequeue();
