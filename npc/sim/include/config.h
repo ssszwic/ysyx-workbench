1df9605898f222292c1854885f67a9453ac0bf2f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-17 17:41:43 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:41:43 up  3:55,  1 user,  load average: 1.45, 0.66, 0.61

--- npc/sim/include/config.h
+++ npc/sim/include/config.h
@@ -17,7 +17,7 @@
 #define CONFIG_DEVICE           1
 #define CONFIG_HAS_SERIAL       1
 #define CONFIG_HAS_TIMER        1
-// #define CONFIG_HAS_KEYBOARD     1
+#define CONFIG_HAS_KEYBOARD     1
 #define CONFIG_HAS_VGA          1
 
 // mem
