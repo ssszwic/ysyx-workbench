af2ce5c245d33ac9633b81c402523e852d1bdf2a
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-17 16:27:26 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:27:26 up  2:40,  1 user,  load average: 0.19, 0.58, 0.73

--- npc/sim/src/mem/mem.cpp
+++ npc/sim/src/mem/mem.cpp
@@ -94,12 +94,12 @@ extern "C" void pmem_write(long long waddr, long long wdata, uint8_t wmask) {
 
   uint8_t data_byte;
   if (likely(in_pmem(waddr))) {
-    for (int i = 0; i < 8; i++) {
-      if((wmask >> i) % 2 == 1) {
-        data_byte = (uint8_t) (wdata >> (8 * i)) & 0xFF;
-        host_write(guest_to_host(waddr + i), 1, data_byte);
-      }
-    }
+    // for (int i = 0; i < 8; i++) {
+    //   if((wmask >> i) % 2 == 1) {
+    //     data_byte = (uint8_t) (wdata >> (8 * i)) & 0xFF;
+    //     host_write(guest_to_host(waddr + i), 1, data_byte);
+    //   }
+    // }
     return;
   }
 
