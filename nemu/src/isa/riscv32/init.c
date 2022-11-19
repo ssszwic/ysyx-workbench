d2802f21cdcf8936498dd5db4012da3429951be7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-19 17:36:33 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:36:33 up  5:34,  1 user,  load average: 0.68, 0.28, 0.26

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -31,9 +31,6 @@ static void restart() {
 
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
-
-  /* The csr mstatus register is always 0. */
-  cpu.csr[0] = 0xa00001800;
 }
 
 void init_isa() {
