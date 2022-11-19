abf3fc9084380f865b472b421dd508871b954739
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-19 20:28:21 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:28:21 up  8:26,  1 user,  load average: 0.52, 0.34, 0.23

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("this is tield\n"); c->mepc += 4; break;
+    case EVENT_YIELD: printf("nothing in yield\n"); c->mepc += 4; break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   
