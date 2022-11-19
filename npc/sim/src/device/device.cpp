bbad7a4187e12f9c429ab07f7fa74f20e9df08e3
ssszw <zhwsong66@163.com>
2022-11-19 12:07:49 +0800

    before starting pa3

--- npc/sim/src/device/device.cpp
+++ npc/sim/src/device/device.cpp
@@ -25,24 +25,24 @@ void init_device() {
 void device_update() {
   IFDEF(CONFIG_HAS_VGA, vga_update_screen());
 
-//   SDL_Event event;
-//   while (SDL_PollEvent(&event)) {
-//     switch (event.type) {
-//       case SDL_QUIT:
-//         npc_state.state = NPC_QUIT;
-//         break;
-// #ifdef CONFIG_HAS_KEYBOARD
-//       // If a key was pressed
-//       case SDL_KEYDOWN:
-//       case SDL_KEYUP: {
-//         uint8_t k = event.key.keysym.scancode;
-//         bool is_keydown = (event.key.type == SDL_KEYDOWN);
-//         send_key(k, is_keydown);
-//         break;
-//       }
-// #endif
-//       default: break;
-//     }
-//   }
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        npc_state.state = NPC_QUIT;
+        break;
+#ifdef CONFIG_HAS_KEYBOARD
+      // If a key was pressed
+      case SDL_KEYDOWN:
+      case SDL_KEYUP: {
+        uint8_t k = event.key.keysym.scancode;
+        bool is_keydown = (event.key.type == SDL_KEYDOWN);
+        send_key(k, is_keydown);
+        break;
+      }
+#endif
+      default: break;
+    }
+  }
 }
 
