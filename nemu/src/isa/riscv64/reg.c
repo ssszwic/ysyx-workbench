10abcf9d124cd97a7d27be64366d12f638ce1d7b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-19 18:29:04 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:29:04 up  6:26,  1 user,  load average: 0.34, 0.39, 0.27

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -20,20 +20,26 @@ const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
+  "mstatus", "mtvec", "mepc", "mcause"
 };
 
 void isa_reg_display() {
-  int i;
   // pc reg
-  printf("%-12s", "pc");
-  printf("0x%016lx    ", cpu.pc);
-  printf("%ld\n", cpu.pc);
+  printf(ANSI_FMT("CPU register state: \n", ANSI_FG_BLUE));
+  printf("pc:  0x%016lx\t  ", cpu.pc);
+
   // normal 32 reg
-  for (i = 0; i < 32; i++) {
-    printf("%-12s", reg_name(i, 0));
-    printf("0x%016lx    ", gpr(i));
-    printf("%ld\n", gpr(i));
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 4; j++) {
+      printf("%-5s0x%016lx   ", regs[i + 8 * j], cpu.gpr[i + 8 * j]);
+    }
+    printf("\n");
+  }
+
+  // csr 4 reg
+  for (int i = 0; i < 4; i++) {
+      printf("%-10s0x%016lx\n", regs[i + 32], cpu.csr[i]);
   }
 }
 
