31e512bef71a26b3125c3dfdb5dc00762adfb3e5
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-22 15:28:45 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:28:45 up  4:47,  1 user,  load average: 1.42, 0.51, 0.29

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -4,7 +4,7 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
-static uint64_t timecmp = 0x100;
+// static uint64_t timecmp = 0x100;
 
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
@@ -12,7 +12,7 @@ Context* __am_irq_handle(Context *c) {
 
     switch (c->mcause) {
       case 0xb: ev.event = EVENT_YIELD; break;
-      case 0x7: ev.event = EVENT_IRQ_TIMER; timecmp = timecmp + 0x1000; outd(0x2004000, timecmp);break;
+      // case 0x7: ev.event = EVENT_IRQ_TIMER; timecmp = timecmp + 0x1000; outd(0x2004000, timecmp);break;
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -32,10 +32,10 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
   // open time irq and set timecmp
-  outd(0x200BFF8, 0);
-  outd(0x2004000, timecmp);
-  uint64_t mie = 0x80;
-  asm volatile("csrw mie, %0" : : "r"(mie));
+  // outd(0x200BFF8, 0);
+  // outd(0x2004000, timecmp);
+  // uint64_t mie = 0x80;
+  // asm volatile("csrw mie, %0" : : "r"(mie));
 
   // register event handler
   user_handler = handler;
