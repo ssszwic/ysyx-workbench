58dee6fab1f0aa7687158a6f5c1cd9ff5b55f61c
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 17:13:00 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:13:00 up 1 day,  6:31,  1 user,  load average: 0.56, 0.43, 0.37

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -48,7 +48,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   // read Program Headers
   Elf_Phdr *pstart = (Elf_Phdr*) malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
   assert(pstart != NULL);
-  // ramdisk_read(&pstart, elf_head.e_phoff, sizeof(Elf_Phdr) * elf_head.e_phnum);
+  printf("len: %d\n", sizeof(Elf_Phdr) * elf_head.e_phnum);
+  ramdisk_read(&pstart, elf_head.e_phoff, sizeof(Elf_Phdr) * elf_head.e_phnum);
 
   // print program table type
   printf("program table: num %lx\n", elf_head.e_machine);
