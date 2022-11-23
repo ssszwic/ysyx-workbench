e6c62d065b7acbb483692e2584a5eee707578e53
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 16:45:24 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:45:24 up 1 day,  6:03,  1 user,  load average: 0.32, 0.31, 0.29

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -9,26 +9,16 @@ void init_proc(void);
 
 int main() {
   extern const char logo[];
-
   printf("%s", logo);
-
-  uint64_t *tmp2 = malloc(8);
-  *tmp2 = 10;
-  printf("tmp2: %lx\n", *tmp2);
 
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
-  
 
   init_mm();
 
-
-
   init_device();
   
-  
-
   init_ramdisk();
 
 #ifdef HAS_CTE
