5d513c9f339a6fca91678fc56c13efb875ac58dc
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 11:53:48 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:53:48 up 1 day,  1:12,  1 user,  load average: 0.49, 0.34, 0.29

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,6 +9,15 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+// only support riscv and native(x86_64)
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+# define EXPECT_TYPE EM_RISCV
+#else
+# error Unsupported ISA
+#endif
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
@@ -16,14 +25,27 @@ size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_head;
   ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
-  printf("magic: ");
-  for(int i = 0; i < 16; i++) {
-    printf("%x ", elf_head.e_ident[i]);
-  }
-  printf("\n");
-  assert(0);
+
+  // check majic
+  if (elf_head.e_ident[0] != 0x7F || elf_head.e_ident[1] != 'E' || 
+      elf_head.e_ident[2] != 'L' || elf_head.e_ident[3] != 'F')
+	{
+		printf("Not a ELF file\n");
+		assert(0);
+	}
+
+  // printf("magic: ");
+  // for(int i = 0; i < 16; i++) {
+  //   printf("%x ", elf_head.e_ident[i]);
+  // }
+  // printf("\n");
+  // assert(0);
+
+  // check ARCH
+  assert(elf_head.e_machine == EXPECT_TYPE);
   
   
+  printf("ok\n");
   return 0;
 }
 
