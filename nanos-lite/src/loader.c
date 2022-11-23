896944f0c214f2205e2e4e6278b0714f6490d940
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 11:26:58 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:26:58 up 1 day, 45 min,  1 user,  load average: 0.22, 0.34, 0.26

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,14 +14,14 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr elf_head;
-  ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
-  printf("magic: ");
-  for(int i = 0; i < 16; i++) {
-    printf("%x \n", elf_head.e_ident[i]);
-  }
-  printf("\n");
-  assert(0);
+  // Elf_Ehdr elf_head;
+  // ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
+  // printf("magic: ");
+  // for(int i = 0; i < 16; i++) {
+  //   printf("%x \n", elf_head.e_ident[i]);
+  // }
+  // printf("\n");
+  // assert(0);
   
   
   return 0;
