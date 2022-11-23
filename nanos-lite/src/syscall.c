d9fc47cbf59f8fa71c34dd15fef27e31fb454f56
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 20:30:32 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:30:32 up 1 day,  9:48,  1 user,  load average: 0.18, 0.20, 0.12

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,6 +6,7 @@ void sys_yield(Context *c);
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1; // type
+  printf("type: %lx\n", a[0]);
 
   switch (a[0]) {
     case SYS_yield: sys_yield(c); break;
