405ca32a5e4fc916efb79185eda3a6843a305792
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 15:42:46 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:42:46 up 1 day,  5:01,  1 user,  load average: 0.24, 0.26, 0.15

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,7 +5,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-static char *hbrk;
+static char *hbrk = (void *)0;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
