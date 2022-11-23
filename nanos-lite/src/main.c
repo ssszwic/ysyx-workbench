d7ab109a2a0e08c16f284e4a8490ea6bd12521a8
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 16:52:32 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:52:32 up 1 day,  6:10,  1 user,  load average: 0.46, 0.34, 0.29

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -9,8 +9,9 @@ void init_proc(void);
 
 int main() {
   extern const char logo[];
-  printf("%s", logo);
 
+  printf("%s", logo);
+  
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
