64abcfb322bd5edc4c87a5aeef7afb27e079bfc7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 11:37:47 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:37:47 up  1:29,  1 user,  load average: 0.87, 0.48, 0.29

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,7 +1,7 @@
 #include <common.h>
 #include "syscall.h"
 
-#define CONFIG_SYSTEMCALL_TRACE
+// #define CONFIG_SYSTEMCALL_TRACE
 
 #ifdef CONFIG_SYSTEMCALL_TRACE
 #define SYSTEMCALL_RING_BUF_WIDTH 30
@@ -37,7 +37,6 @@ void do_syscall(Context *c) {
   if (++systemcall_ring_ref == SYSTEMCALL_RING_BUF_WIDTH) {systemcall_ring_ref = 0;}
   sprintf(tmp, "----> ID: %2d  para: 0x%lx  0x%lx  0x%lx  return: 0x%lx", a[0], a[1], a[2], a[3], c->GPRx);
   strcpy(systemcall_ring_buf[systemcall_ring_ref], tmp);
-  printf("%s\n", tmp);
 #endif
 }
 
