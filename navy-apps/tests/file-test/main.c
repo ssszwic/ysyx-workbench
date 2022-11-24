4f478099ea70d7d899e694f5b4342a70ecb934da
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 15:55:58 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:55:58 up  5:48,  1 user,  load average: 0.50, 0.31, 0.26

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -4,7 +4,7 @@
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
-  printf("fp: %d\n", (uint64_t) *fp);
+  printf("fp: %ld\n", *fp);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
