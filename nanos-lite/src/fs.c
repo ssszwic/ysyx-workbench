2fd6cb9cbdb8bf33dc798971fde3f21771d34235
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 16:10:03 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:10:03 up  6:02,  1 user,  load average: 0.42, 0.27, 0.27

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -69,6 +69,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
+  printf("cfo: %x\n", file_table[fd].cfo);
   assert(fd > 0 && fd < file_num);
   if(fd < 3) {
     // print to stdout
