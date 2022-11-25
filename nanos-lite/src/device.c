310a40e97ea0a8bbd0c7af6ff909d87364c405a4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-25 17:08:44 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:08:44 up  6:10,  1 user,  load average: 0.49, 0.43, 0.37

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,9 +23,32 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
   return len;
 }
 
+#ifdef CONFIG_SYSTEMCALL_TRACE
+  char tmp[MAX_SINGLE_SYSTEMCALL_WIDTH] = {};
+  memset(systemcall_ring_buf[systemcall_ring_ref], ' ', 6);
+  if (++systemcall_ring_ref == SYSTEMCALL_RING_BUF_WIDTH) {systemcall_ring_ref = 0;}
+  sprintf(tmp, "----> ID: %2d  para: 0x%lx  0x%lx  0x%lx  return: 0x%lx", a[0], a[1], a[2], a[3], c->GPRx);
+  strcpy(systemcall_ring_buf[systemcall_ring_ref], tmp);
+#endif
+
 size_t events_read(void *buf, size_t offset, size_t len) {
-  
-  return 0;
+  int i = 0;
+  char tmp[10] = {};
+  while(1) {
+    AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+    if (ev.keycode == AM_KEY_NONE) break;
+    if(ev.keydown) {
+      sprintf(tmp, "kd %s\n", keyname[ev.keycode]);
+    }
+    else {
+      sprintf(tmp, "ku %s\n", keyname[ev.keycode]);
+    }
+    // the event beyond the buf will be discarded
+    if(i + strlen(tmp) > len) break;
+    strcpy((char *)buf + i, tmp);
+    i += strlen(tmp);
+  }
+  return i;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
