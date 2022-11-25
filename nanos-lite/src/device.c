3f96491a8b43cf0b51ce513d610a7df326373724
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-25 14:00:34 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:00:34 up  3:02,  1 user,  load average: 0.45, 0.31, 0.17

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,12 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  // print to stdout
+  char *str = (char *) buf;
+  for(int i = 0; i < len; i++) {
+    putch(str[i]);
+  }
+  return len;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -28,6 +28,7 @@ enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t serial_write(const void *buf, size_t offset, size_t len);
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -39,20 +40,11 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
-size_t std_write(const void *buf, size_t offset, size_t len) {
-  // print to stdout
-  char *str = (char *) buf;
-  for(int i = 0; i < len; i++) {
-    putch(str[i]);
-  }
-  return len;
-}
-
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, std_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, std_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -169,9 +161,9 @@ int fs_close(int fd) {
   strcpy(file_ring_buf[file_ring_ref], tmp);
   #endif
 
-  #ifdef CONFIG_FILE_TRACE
-  print_file_log();
-  #endif
+  // #ifdef CONFIG_FILE_TRACE
+  // print_file_log();
+  // #endif
 
   return 0;
 }
