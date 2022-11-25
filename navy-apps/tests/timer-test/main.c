9a868b846be15833a8232e7495027255b556bcb7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-25 16:08:47 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:08:47 up  5:10,  1 user,  load average: 0.52, 0.40, 0.22

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <sys/time.h>
+#include <NDL.h>
 
 int main() {
-  struct timeval tv_begin, tv_end;
-  gettimeofday(&tv_begin, NULL);
-
+  uint32_t begin, end;
+  begin = NDL_GetTicks();
   while(1) {
-    gettimeofday(&tv_end, NULL);
-    if(tv_end.tv_usec - tv_begin.tv_usec > 500000) {
-      tv_begin = tv_end;
+    end = NDL_GetTicks();
+    if(end - begin > 500) {
+      begin = end;
       printf("0.5s have gone\n");
     }
   }
