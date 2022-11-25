ecbca1a618cffdcc2a681cef8b3faa716d8f3274
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-25 14:56:05 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:56:05 up  3:57,  1 user,  load average: 0.34, 0.27, 0.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -98,15 +98,9 @@ void sys_brk(Context *c) {
 }
 
 void sys_gettimeofday(Context *c) {
-  printf("addr: %lx\n", io_read(AM_TIMER_UPTIME).us);
-  printf("1\n");
   *(uint64_t *) c->GPR2 = io_read(AM_TIMER_UPTIME).us;
-  printf("2\n");
-  // *(((uint64_t *) c->GPR2) + 1) = (*(uint64_t *) c->GPR2) / 1000000;
-  printf("3\n");
+  *(((uint64_t *) c->GPR2) + 1) = (*(uint64_t *) c->GPR2) / 1000000;
   c->GPRx = 0;
-  printf("%ld\n", *(uint64_t *) c->GPR2);
-  printf("4\n");
   return;
 }
 
