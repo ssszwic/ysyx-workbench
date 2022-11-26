3dc38a0d41601e57ac6c72fb50d02e936fdb1913
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-26 19:48:27 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:48:27 up  9:29,  1 user,  load average: 0.57, 0.61, 0.66

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -33,13 +33,14 @@ static void draw_ch(int x, int y, char ch, uint32_t fg, uint32_t bg) {
   SDL_Rect dstrect = { .x = x, .y = y };
   SDL_BlitSurface(s, NULL, screen, &dstrect);
   
-  for(int j = 0; j < s->h; j++) {
-    for (int i = 0; i < s->w; j++) {
-      printf("%x ", s->pixels + j * s->w + i);
-    }
-    printf("\n");
-  }
-  printf("\n");
+  printf("%d, %d\n", s->w, s->h);
+  // for(int j = 0; j < s->h; j++) {
+  //   for (int i = 0; i < s->w; j++) {
+  //     printf("%x ", s->pixels + j * s->w + i);
+  //   }
+  //   printf("\n");
+  // }
+  // printf("\n");
 
   SDL_FreeSurface(s);
 
@@ -50,7 +51,7 @@ void refresh_terminal() {
   for (int i = 0; i < W; i ++)
     for (int j = 0; j < H; j ++)
       if (term->is_dirty(i, j)) {
-        draw_ch(i * font->w, j * font->h, term->getch(i, j), term->foreground(i, j), term->background(i, j));
+        // draw_ch(i * font->w, j * font->h, term->getch(i, j), term->foreground(i, j), term->background(i, j));
         needsync = 1;
       }
   term->clear();
