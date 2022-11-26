59a4c083648a8fe46feb8445556d6add96a0c551
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-26 19:55:57 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:55:57 up  9:37,  1 user,  load average: 0.26, 0.64, 0.70

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -32,12 +32,22 @@ static void draw_ch(int x, int y, char ch, uint32_t fg, uint32_t bg) {
   SDL_Surface *s = BDF_CreateSurface(font, ch, fg, bg);
   SDL_Rect dstrect = { .x = x, .y = y };
   SDL_BlitSurface(s, NULL, screen, &dstrect);
-  uint32_t *tmp = (uint32_t *) s->pixels;
+
+  uint32_t *tmp = (uint32_t *) screen->pixels;
   
+  // printf("%d, %d\n", s->w, s->h);
+  // for(int j = 0; j < s->h; j++) {
+  //   for (int i = 0; i < s->w; i++) {
+  //     printf("%x ", * tmp + j * s->w + i);
+  //   }
+  //   printf("\n");
+  // }
+  // printf("\n");
+
   printf("%d, %d\n", s->w, s->h);
   for(int j = 0; j < s->h; j++) {
     for (int i = 0; i < s->w; i++) {
-      printf("%x ", * tmp + j * s->w + i);
+      printf("%x ", * tmp + screen->w * (j + y) + x + i);
     }
     printf("\n");
   }
