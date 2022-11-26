c7b2c66e2d6f73f6f1b46e782899f5339c5856cb
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-26 19:30:15 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:30:15 up  9:11,  1 user,  load average: 0.83, 0.82, 0.73

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -55,7 +55,7 @@ void refresh_terminal() {
     uint32_t color = (flip ? term->foreground(x, y) : term->background(x, y));
     draw_ch(x * font->w, y * font->h, ' ', 0, color);
 
-    if(tmp % 3 == 0) SDL_FillRect(screen, NULL, 0);
+    if(tmp % 3 == 0) SDL_FillRect(screen, NULL, 0x00ffffff);
     tmp++;
 
     
