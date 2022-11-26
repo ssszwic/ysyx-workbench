83e350f680e37cbf7ab1d405cd9c9a2c742bcb54
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-26 20:42:27 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:42:27 up 10:23,  1 user,  load average: 0.60, 0.69, 0.70

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -34,15 +34,15 @@ static void draw_ch(int x, int y, char ch, uint32_t fg, uint32_t bg) {
   SDL_Rect dstrect = { .x = x, .y = y };
   
 
-  // uint32_t *tmp1 = (uint32_t *) s->pixels;
-  // printf("%d, %d\n", s->w, s->h);
-  // for(int j = 0; j < s->h; j++) {
-  //   for (int i = 0; i < s->w; i++) {
-  //     printf("%x ", * tmp1 + j * s->w + i);
-  //   }
-  //   printf("\n");
-  // }
-  // printf("\n");
+  uint32_t *tmp1 = (uint32_t *) s->pixels;
+  printf("%d, %d\n", s->w, s->h);
+  for(int j = 0; j < s->h; j++) {
+    for (int i = 0; i < s->w; i++) {
+      printf("%x ", * tmp1 + j * s->w + i);
+    }
+    printf("\n");
+  }
+  printf("\n");
 
 
   
