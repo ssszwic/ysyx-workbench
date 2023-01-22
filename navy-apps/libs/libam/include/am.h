99447cb0aaff67b9e987c3deda66f080176dd07d
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-22 17:39:17 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:39:17 up 1 min,  1 user,  load average: 0.98, 0.34, 0.12

--- navy-apps/libs/libam/include/am.h
+++ navy-apps/libs/libam/include/am.h
@@ -4,4 +4,13 @@
 #define ARCH_H "navy.h"
 #include "am-origin.h"
 
+#define io_read(reg) \
+  ({ reg##_T __io_param; \
+    ioe_read(reg, &__io_param); \
+    __io_param; })
+
+#define io_write(reg, ...) \
+  ({ reg##_T __io_param = (reg##_T) { __VA_ARGS__ }; \
+    ioe_write(reg, &__io_param); })
+
 #endif
--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -2,8 +2,14 @@
 
 Area heap;
 
+# define nemu_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+
 void putch(char ch) {
+  int fd = open("stdout", 0, 0);
+  write(fd, ch, 1);
+  close(fd)
 }
 
 void halt(int code) {
+  nemu_trap(code);
 }
