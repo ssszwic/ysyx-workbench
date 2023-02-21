b6060e062081fce27085a88503b7ee235ca199bc
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-21 21:33:17 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:33:17 up  2:24,  1 user,  load average: 0.39, 0.50, 0.46

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -21,6 +21,7 @@
 #include <utils.h>
 #include <difftest-def.h>
 
+
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
@@ -40,7 +41,7 @@ void attach() {
   stop_diff = false;
   // copy memory and reg
   // copy memory to ref
-  ref_difftest_memcpy(0x100000, guest_to_host(0x100000), PMEM_SIZE - 0x100000, DIFFTEST_TO_REF);
+  ref_difftest_memcpy(0x100000, guest_to_host(0x100000), CONFIG_MSIZE - 0x100000, DIFFTEST_TO_REF);
   // copy reg to ref
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
