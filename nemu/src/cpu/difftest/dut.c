ff8a89409289d8a002c449d6dc48177a6e688170
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-21 21:15:38 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:15:38 up  2:06,  1 user,  load average: 0.68, 0.44, 0.33

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -26,8 +26,19 @@ void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
+void detach();
+void attach();
+
 static bool is_skip_ref = false;
 static int skip_dut_nr_inst = 0; // for QEMU
+
+void detach() {
+  printf("detach\n");
+}
+
+void attach() {
+  printf("attach\n");
+}
 
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
