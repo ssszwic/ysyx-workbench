b54bd774a7d4cce464447dfef459e2de673b42b9
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-21 21:39:12 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.15.0-60-generic #66-Ubuntu SMP Fri Jan 20 14:29:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:39:12 up  2:30,  1 user,  load average: 0.56, 0.51, 0.47

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -118,7 +118,6 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   if(stop_diff) return;
-  printf("diff\n");
   CPU_state ref_r;
 
   // for QEMU, let dut catch up ref
