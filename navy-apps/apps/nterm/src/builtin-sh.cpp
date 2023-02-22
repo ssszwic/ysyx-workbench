7d95a09136bab5e3743e23b2b977c24d7e6e1387
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-22 19:00:14 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:00:14 up  1:52,  1 user,  load average: 0.60, 0.71, 0.52

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -23,6 +23,7 @@ static void sh_prompt() {
 }
 
 static void sh_handle_cmd(const char *cmd) {
+  execve(cmd, NULL, NULL);
 }
 
 void builtin_sh_run() {
@@ -34,8 +35,6 @@ void builtin_sh_run() {
     if (SDL_PollEvent(&ev)) {
       if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
         const char *res = term->keypress(handle_key(&ev));
-        printf("666");
-        printf("%s", res);
         if (res) {
           sh_handle_cmd(res);
           sh_prompt();
