04a3fad55cdeee91fc75d504b2a194627d132859
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-22 17:47:42 +0800

    > compile NEMU ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:47:42 up 40 min,  1 user,  load average: 0.81, 0.57, 0.41

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -116,7 +116,8 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
-  _exit(SYS_execve);
+  // _exit(SYS_execve);
+  _syscall_(SYS_execve, (intptr_t) fname, 0, 0);
   return 0;
 }
 
