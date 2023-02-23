fa68eb52574e3f03a04e25234feafd4907bedebb
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-23 16:51:17 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:51:17 up 11 min,  1 user,  load average: 1.01, 0.55, 0.29

--- npc/sim/src/device/vga.cpp
+++ npc/sim/src/device/vga.cpp
@@ -43,9 +43,9 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
-  if (vgactl_port_base[0] == 1) {
+  if (vgactl_port_base[1] == 1) {
     update_screen();
-    vgactl_port_base[0] = 0;
+    vgactl_port_base[1] = 0;
   }
 }
 
