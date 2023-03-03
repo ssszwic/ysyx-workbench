b6a106f55fd056b84cf954bd240a7e09782ff49f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 15:20:23 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:23 up 1 day, 22:20,  1 user,  load average: 1.02, 0.72, 0.64

--- npc/playground/src/main/Elaborate.scala
+++ npc/playground/src/main/Elaborate.scala
@@ -9,14 +9,28 @@ import main.LSU
 import main.MEM
 import main.WBU
 
+class MemInterface extends Bundle {
+  // read
+  val ren   = Input(Bool())
+  val addr  = Input(UInt(64.W))
+  val rData = Output(UInt(64.W))
+  // write
+  val wen   = Input(Bool())
+  val wData = Input(UInt(64.W))
+  val wMask = Input(UInt(8.W))
+}
+
 object Elaborate extends App {
-  def top = new IDU.IDU
-  val useMFC = true // use MLIR-based firrtl compiler
-  val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  if (useMFC) {
-    (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
-  } else {
-    (new chisel3.stage.ChiselStage).execute(args, generator)
-  }
+  val a = new MemInterface
+  print(a)
+
+  // def top = new IDU.IDU
+  // val useMFC = true // use MLIR-based firrtl compiler
+  // val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
+  // if (useMFC) {
+  //   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+  // } else {
+  //   (new chisel3.stage.ChiselStage).execute(args, generator)
+  // }
   // (new ChiselStage).emitVerilog(top, args)
 }
