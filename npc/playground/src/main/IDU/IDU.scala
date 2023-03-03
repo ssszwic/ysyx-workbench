5b4c792db590103617fd2e032aecf0f39241aaaf
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 13:17:07 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:07 up 1 day, 20:16,  1 user,  load average: 0.78, 1.06, 0.92

--- npc/playground/src/main/IDU/IDU.scala
+++ npc/playground/src/main/IDU/IDU.scala
@@ -11,50 +11,58 @@ import main.IFU
 class IDUInterface extends Bundle {
   val ready = Input(Bool())
   val valid = Output(Bool())
-  val rs1Data = Output(UInt(64.W))
-  val rs2Data = Output(UInt(64.W))
-  val imme    = Output(UInt(64.W))
-  val pc      = Output(UInt(64.W))
-  val pc4     = Output(UInt(64.W))
-  val aluCtrl = Flipped(new EXU.AluCtrlInterface)
-  val regCtrl = Flipped(new IDU.RegCtrlInterface)
-  val memCtrl = Flipped(new LSU.MemCtrlInterface)
-  val csrCtrl = Flipped(new EXU.CSRCtrlInterface)
+  // val rs1Data = Output(UInt(64.W))
+  // val rs2Data = Output(UInt(64.W))
+  // val imme    = Output(UInt(64.W))
+  // val pc      = Output(UInt(64.W))
+  // val pc4     = Output(UInt(64.W))
+  // val aluCtrl = Flipped(new EXU.AluCtrlInterface)
+  // val regCtrl = Flipped(new IDU.RegCtrlInterface)
+  // val memCtrl = Flipped(new LSU.MemCtrlInterface)
+  // val csrCtrl = Flipped(new EXU.CSRCtrlInterface)
 }
 
 class IDU extends Module{
-  // val ioIFU       = IO(Flipped(new IFU.IFUInterface))
-  // val ioIDU       = IO(new IDUInterface)
-  // // from LSU
+  val ioIFU       = IO(Flipped(new IFU.IFUInterface))
+  val ioIDU       = IO(new IDUInterface)
+  // from LSU
   // val ioLSU = new Bundle {
   //   val regCtrl = IO(new IDU.RegCtrlInterface)
   //   val rdData  = Input(UInt(64.W))
   // }
-  val io = IO(new Bundle {
-    val in = Input(UInt(4.W))
-    val out = Output(UInt(4.W))
-  })
+
+  // val io = IO(new Bundle {
+  //   val 
+  // })
 
   // FSM
-  // val sIDLE :: sFINISH :: Nil = Enum(2)
-  // val state = RegInit(sIDLE)
+  val sIDLE :: sFINISH :: Nil = Enum(2)
+  val state = RegInit(sIDLE)
 
-  // switch(state) {
-  //   is(sIDLE) {
-  //     when(ioIFU.valid) {
-  //       state := sFINISH
-  //     }.otherwise {
-  //       state := sIDLE
-  //     }
-  //   }
-  //   is(sFINISH) {
-  //     when(ioIDU.ready) {
-  //       state := sIDLE
-  //     }.otherwise {
-  //       state := sFINISH
-  //     }
-  //   }
-  // }
-  io.out := RegNext(io.in, init = 0.U(4.W))
+  switch(state) {
+    is(sIDLE) {
+      when(ioIFU.valid) {
+        state := sFINISH
+        ioIFU.ready := false.B
+        ioIDU.valid := true.B
+      }.otherwise {
+        state := sIDLE
+        ioIFU.ready := true.B
+        ioIDU.valid := false.B
+      }
+    }
+    is(sFINISH) {
+      when(ioIDU.ready) {
+        state := sIDLE
+        ioIFU.ready := true.B
+        ioIDU.valid := false.B
+      }.otherwise {
+        state := sFINISH
+        ioIFU.ready := false.B
+        ioIDU.valid := true.B
+      }
+    }
+  }
+
 }
 
