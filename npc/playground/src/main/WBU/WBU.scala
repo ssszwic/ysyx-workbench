d45dda489810cbaa41cee1b2ef36ccfb26ddacd6
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-05 14:38:30 +0800

    > generate verilog ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:38:30 up  2:18,  1 user,  load average: 1.46, 1.70, 1.52

--- npc/playground/src/main/WBU/WBU.scala
+++ npc/playground/src/main/WBU/WBU.scala
@@ -25,7 +25,7 @@ class WBU extends Module {
 
   val regEn = Wire(Bool())
   regEn := (state === sIDLE) && ioLSU.valid
-  ioWBU.npc := RegEnable(ioLSU.npc, 0x080000000.U, regEn)
+  ioWBU.npc := RegEnable(ioLSU.npc, "h80000000".U, regEn)
   ioWBU.ioReg.regCtrl.wen   := Mux(regEn, ioLSU.regCtrl.wen, false.B)
   ioWBU.ioReg.regCtrl.rdAddr := ioLSU.regCtrl.rdAddr
   when(ioLSU.csrSel) {
