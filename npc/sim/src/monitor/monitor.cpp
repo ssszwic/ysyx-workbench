d633013052b8c6fe546aa1e4f234fd3f699b3964
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-11 19:57:28 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:28 up 1 day,  2:51,  1 user,  load average: 0.26, 0.35, 0.31

--- npc/sim/src/monitor/monitor.cpp
+++ npc/sim/src/monitor/monitor.cpp
@@ -11,6 +11,7 @@ static char *diff_file = NULL;
 
 static const uint32_t img [] = {
   0x00000297,  // auipc t0,0
+  0x00000517,  // auipc a0,0
   0x0002b823,  // sd  zero,16(t0)
   0x0102b503,  // ld  a0,16(t0)
   0x00100073,  // ebreak (used as nemu_trap)
