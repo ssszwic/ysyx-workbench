e18f699f2f374c8acb298ecb104eb474edf309cb
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-11 11:50:06 +0800

    > sim RTL ysyx_22050610 宋政伟 Linux ssszw-Ubuntu 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:50:06 up 18:43,  1 user,  load average: 0.69, 0.55, 0.77

--- npc/sim/include/config.h
+++ npc/sim/include/config.h
@@ -4,7 +4,7 @@
 // must define 1 or 0 if use IFDEF...
 // switch
 #define CONFIG_WAVE             1
-// #define CONFIG_ITRACE           1
+#define CONFIG_ITRACE           1
 // #define CONFIT_WATCHPOINT       1//depend CONFIG_ITRACE
 // #define CONFIG_MEMORY_TRACE     1
 // #define CONFIG_FUNCTION_TRACE   1
